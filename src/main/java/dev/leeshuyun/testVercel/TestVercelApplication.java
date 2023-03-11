44a46fadaaea97610243b8875c171718e8bfa10c
LeeShuYun <quaternion.design@gmail.com>
2023-03-11 19:35:55 +0800

    first commit

---
+++ src/main/java/dev/leeshuyun/testVercel/TestVercelApplication.java
@@ -0,0 +1,13 @@
+package dev.leeshuyun.testVercel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVercelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestVercelApplication.class, args);
+	}
+
+}
